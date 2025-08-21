8e41416a0766de7865f9709ef42968a1d8530a69
Yohan <io.yohanlee@gmail.com>
2025-08-21 17:59:35 -0400

    refactor cell colors

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,9 +1,45 @@
-square = "⬛️"
+class Cell
+  COLORS = {
+    dead: "⬛️",
+    alive: "⬜️"
+  }
+
+  def self.colors(color)
+    COLORS[color]
+  end
+
+  attr_reader :state
+
+  def initialize(state = :dead)
+    @state = state
+  end
+
+  def dead?
+    @state == :dead
+  end
+
+  def alive?
+    @state == :alive
+  end
+
+  def dead!
+    @state = :dead
+  end
+
+  def alive!
+    @state = :alive
+  end
+
+  def render
+    COLORS[state]
+  end
+end
+
 width = 40
 height = (width * 0.6).round
 
 # loop do
-  grid = height.times.map { Array.new(width, square) }
+  grid = height.times.map { Array.new(width, Cell.colors(:dead)) }
   # returns x,y coordinates of the center
   # grid.length = outer array aka height
   center_coord = [(width / 2).round, (grid.length / 2).round]
@@ -14,7 +50,7 @@ height = (width * 0.6).round
     x, y = seed_coord
     # in order to access values in two dimensional array you have to
     # go in the reverse order y and x because outer array represents the height
-    grid[center_y + y][center_x + x] = "⬜️"
+    grid[center_y + y][center_x + x] = Cell.colors(:alive)
   end
   output = grid.map { _1.join("") }.join("\n")
   File.write("output.txt", output)
